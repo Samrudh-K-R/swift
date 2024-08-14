e21e4f9070983332e931186b1726b901d6771b70
Anton Korobeynikov <anton@korobeynikov.info>
2024-08-13 22:27:02 -0700

    First cut of making coroutine AST type

--- lib/Serialization/Serialization.cpp
+++ lib/Serialization/Serialization.cpp
@@ -5698,8 +5698,13 @@ public:
     visitBuiltinTypeImpl(ty);
   }
 
-  void visitYieldResultType(const YieldResultType *) {
-    llvm_unreachable("implemented me");
+  void visitYieldResultType(const YieldResultType *ty) {
+    using namespace decls_block;
+    unsigned abbrCode = S.DeclTypeAbbrCodes[YieldResultTypeLayout::Code];
+
+    YieldResultTypeLayout::emitRecord(S.Out, S.ScratchRecord, abbrCode,
+                                      S.addTypeRef(ty->getResultType()),
+                                      ty->isInOut());
   }
 
   void visitTypeAliasType(const TypeAliasType *alias) {
@@ -5991,7 +5996,8 @@ public:
         S.addTypeRef(fnTy->getThrownError()),
         getRawStableDifferentiabilityKind(fnTy->getDifferentiabilityKind()),
         isolation,
-        fnTy->hasSendingResult());
+        fnTy->hasSendingResult(),
+        fnTy->isCoroutine());
 
     serializeFunctionTypeParams(fnTy);
 
@@ -6013,7 +6019,7 @@ public:
         fnTy->isSendable(), fnTy->isAsync(), fnTy->isThrowing(),
         S.addTypeRef(fnTy->getThrownError()),
         getRawStableDifferentiabilityKind(fnTy->getDifferentiabilityKind()),
-        isolation, fnTy->hasSendingResult(),
+        isolation, fnTy->hasSendingResult(), fnTy->isCoroutine(),
         S.addGenericSignatureRef(genericSig));
 
     serializeFunctionTypeParams(fnTy);
@@ -6450,6 +6456,8 @@ void Serializer::writeAllDeclsAndTypes() {
   registerDeclTypeAbbr<DeclNameRefLayout>();
 
   registerDeclTypeAbbr<ClangTypeLayout>();
+
+  registerDeclTypeAbbr<YieldResultTypeLayout>();
 
   registerDeclTypeAbbr<TypeAliasLayout>();
   registerDeclTypeAbbr<GenericTypeParamTypeLayout>();
